fbd09fb7aa5eae091c31879d9586e6f126265a38
Kunal Sareen <kunal.sareen@anu.edu.au>
2023-03-21 18:19:34 +0000

    Fix fastdebug extract path

--- .github/scripts/build-normal.sh
+++ .github/scripts/build-normal.sh
@@ -12,3 +12,9 @@ cd $OPENJDK_PATH
 sh configure --disable-warnings-as-errors --with-debug-level=$DEBUG_LEVEL
 make CONF=linux-x86_64-normal-server-$DEBUG_LEVEL THIRD_PARTY_HEAP=$BINDING_PATH/openjdk product-bundles
 
+if [[ $DEBUG_LEVEL == "fastdebug" ]]; then
+    pushd build/linux-x86_64-normal-server-fastdebug/bundles
+    F=`ls *_bin-debug.tar.gz`
+    mv $F ${F/_bin-debug/_bin}
+    popd
+fi
