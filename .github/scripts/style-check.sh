c03f90bc8529d42e57ddf5f7ac68dab5037eb7fa
Kunal Sareen <kunal.sareen@anu.edu.au>
2023-03-21 09:45:12 +0000

    Make build CI

---
+++ .github/scripts/style-check.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -xe
+
+. $(dirname "$0")/common.sh
+
+export RUSTFLAGS="-D warnings"
+
+pushd $BINDING_PATH/mmtk
+cargo clippy
+cargo clippy --release
+
+cargo fmt -- --check
+popd
+
+find $BINDING_PATH/openjdk \
+    $BINDING_PATH/mmtk \
+    -name '*.hpp' \
+    -o -name '*.cpp' \
+    -o -name '*.rs' \
+    -o -name '*.toml' \
+    -o -name '*.gmk' \
+    -exec ./check-lineends.sh '{}' \;
+
