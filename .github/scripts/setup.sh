d8b4e5d5aacd62ed8f35579473779188852e461c
Kunal Sareen <kunal.sareen@anu.edu.au>
2023-03-21 19:31:58 +0000

    Install dos2unix

--- .github/scripts/setup.sh
+++ .github/scripts/setup.sh
@@ -16,4 +16,5 @@ pip3 install running-ng
 
 # Install dependencies
 sudo apt-get update -y
+sudo apt-get dos2unix
 sudo apt-get install build-essential libx11-dev libxext-dev libxrender-dev libxtst-dev libxt-dev libcups2-dev libasound2-dev libxrandr-dev
